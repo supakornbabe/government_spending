c60f045cb67e8a4d5755f839e80f0dd0e72b2915
Supakorn Wongsawang <supakorn.wo@ku.th>
2019-05-17 01:30:07 +0700

    dasf

--- get_data/python/get_data_2561.py
+++ get_data/python/get_data_2561.py
@@ -13,7 +13,7 @@ def get_with_offset(offset, file):
         'budget_end': '1000000000000',
         'min_code': '15000',
         'offset': offset,
-        'user_token': '402751371d8d559f36046d26b9f964d8',
+        'user_token': token,
         'year': year,
         'prov_code': '03100000'
     }
@@ -28,7 +28,7 @@ def get_with_offset(offset, file):
 
 if __name__ == "__main__":
     for i in range(start, stop+1, 20):
-        filename = '../data/'+str(year)+'/'+str(i)+'.txt'
+        filename = '../data/'+str(year)+'/'+str(i)+'.json'
         f = open(filename, 'w')
         print(i)
         get_with_offset(i, f)
