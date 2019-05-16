458184493a65ec10104933da814253f2037cda20
Supakorn Wongsawang <supakorn.wo@ku.th>
2019-05-17 00:59:38 +0700

    py

--- get_data/index.ts
+++ get_data/index.ts
@@ -1,4 +1,24 @@
-let params = {
-  test: '1234',
+import axios from 'axios';
+import {writeFile} from "fs"
+
+const link = 'https://govspendingapi.data.go.th/api/service/bbgfproject'
+
+const params = {
+    'budget_end' : '1000000',
+    'min_code' : '02000',
+    'offset' : '0',
+    'user_token' : '402751371d8d559f36046d26b9f964d8',
+    'year' : '2561',
 };
-console.log(params.test);
+axios.get(link, {params})
+    .then((response) => {
+        const res = JSON.parse(response.data.result)
+        console.log(res)
+        writeFile('test.txt', res, (err) => {
+            if (err) {
+                throw err;
+            }
+            console.log('saved!');
+        });
+    })
+    .catch((error) => {console.log(error)})
