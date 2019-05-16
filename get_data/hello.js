2925812908a1f0c8bf7dda182dc70731fe837b16
Supakorn Wongsawang <supakorn.wo@ku.th>
2019-05-16 18:04:04 +0700

    test

---
+++ get_data/hello.js
@@ -0,0 +1,5 @@
+function hello(person) {
+  return 'hello ' + person;
+}
+var user = 'babe';
+console.log(hello(user));
---
+++ get_data/hello.ts
@@ -0,0 +1,6 @@
+function hello(person: string) {
+  return 'hello ' + person;
+}
+
+let user = 'babe';
+// console.log(hello(user));
